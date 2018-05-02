ecc65028dbe00e9cc3cc91b114ac4f8b64b9c0b9
Melody Massard <melody.massard8988@gmail.com>
2018-05-02 09:39:19 -0500

    only title clicked on views editing as true

--- src/components/ManagePage/ManagePage.js
+++ src/components/ManagePage/ManagePage.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card, CardMedia, CardHeader, CardText } from 'material-ui/Card'
+// import { Card, CardMedia, CardHeader, CardText } from 'material-ui/Card'
 import Nav from '../../components/Nav/Nav';
-import ProductIems from './ProductItems'
+import ProductItems from './ProductItems'
 
 //passport.js authentication
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
 
 class ManagePage extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { editingTitle: false }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { editingTitle: false }
+  // }
   componentDidMount() {
     //grab specific user's information
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
@@ -30,26 +30,6 @@ class ManagePage extends Component {
     }
   }
 
-  handleEdit = () => {
-    console.log('you clicked edit title!');
-    this.setState({
-      editingTitle: !this.state.editingTitle
-    });
-    console.log(this.state.editingTitle)
-  }
-
-  handleSave = () => {
-    console.log('you clicked save!')
-  }
-
-  handleTitleChange = (event) => {
-    console.log(event.target.value)
-    this.props.dispatch({
-      type: 'UPDATE_TITLE',
-      payload: event.target.value
-    })
-  }
-
   logout = () => {
     this.props.dispatch(triggerLogout());
     // this.props.history.push('home');
@@ -59,46 +39,11 @@ class ManagePage extends Component {
 
     //Map through all products 
     let frontPageProducts = this.props.reduxState.frontReducer.map((product) => {
-
-      //Toggle what title looks like whether it is being edited or not 
-      let title;
-      if (this.state.editingTitle === true) {
-        title = (
-          <div>
-            <textarea defaultValue={product.title} onChange={this.handleTitleChange}></ textarea>
-            <button onClick={this.handleEdit}>Save</button>
-          </div>
-        )
-      } else {
-        title = (<p onClick={this.handleEdit}>{product.title}</p>)
-      }
-
-      //display products as cards
       return (
-        <div key={product.id}>
-          {/* outer card includes title, image, description */}
-          <Card style={{ width: '30%' }} >
-            <CardHeader>
-              {/* <p onClick={this.handleEdit}>{product.title}</p> */}
-              {title}
-            </CardHeader>
-            <CardMedia>
-              <img className="productImages" src={product.image_url} width='60%' alt="" />
-            </CardMedia>
-            <CardText>
-              <p>{product.description}</p>
-            </CardText>
-            {/* inner card includes input form  */}
-            <Card>
-              <p>Message the vendor about this product:</p>
-              <input placeholder="Name"></input><br />
-              <input placeholder="Email"></input><br />
-              <input placeholder="Message"></input><br />
-              <button>Send!</button>
-            </Card>
-          </Card>
-        </div>
-      )
+        <ProductItems
+        key={product.id}
+        product={product}/>
+    )
     })
 
     //if the user is logged in, show a greeting and give log out option
@@ -130,7 +75,7 @@ class ManagePage extends Component {
         <h1>Shop name</h1>
         <h2>Shop info</h2>
         <h3>Shop contact</h3>
-        {/* {frontPageProducts} */}
+        {frontPageProducts}
       </div>
     );
   }
--- src/components/ManagePage/ProductItems.js
+++ src/components/ManagePage/ProductItems.js
@@ -1,9 +1,76 @@
 import React, {Component} from 'react';
-import GalleryItem from '../GalleryItem/GalleryItem'
+import { connect } from 'react-redux';
+import { Card, CardMedia, CardHeader, CardText } from 'material-ui/Card'
 
 
 class ProductItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { editingTitle: false }
+      }
 
+handleEdit = () => {
+    console.log('you clicked edit title!');
+    this.setState({
+      editingTitle: !this.state.editingTitle
+    });
+    console.log(this.state.editingTitle)
+  }
+
+  handleSave = () => {
+    console.log('you clicked save!')
+  }
+
+  handleTitleChange = (event) => {
+    console.log(event.target.value)
+    this.props.dispatch({
+      type: 'UPDATE_TITLE',
+      payload: event.target.value
+    })
+  }
+
+  render() {
+
+   //Toggle what title looks like whether it is being edited or not 
+   let title;
+   if (this.state.editingTitle === true) {
+     title = (
+       <div>
+         <textarea defaultValue={this.props.product.title} onChange={this.handleTitleChange}></ textarea>
+         <button onClick={this.handleEdit}>Save</button>
+       </div>
+     )
+   } else {
+     title = (<p onClick={this.handleEdit}>{this.props.product.title}</p>)
+   }
+
+   //display products as cards
+   return (
+     <div key={this.props.product.id}>
+       {/* outer card includes title, image, description */}
+       <Card style={{ width: '30%' }} >
+         <CardHeader>
+           {/* <p onClick={this.handleEdit}>{product.title}</p> */}
+           {title}
+         </CardHeader>
+         <CardMedia>
+           <img className="productImages" src={this.props.product.image_url} width='60%' alt="" />
+         </CardMedia>
+         <CardText>
+           <p>{this.props.product.description}</p>
+         </CardText>
+         {/* inner card includes input form  */}
+         <Card>
+           <p>Message the vendor about this product:</p>
+           <input placeholder="Name"></input><br />
+           <input placeholder="Email"></input><br />
+           <input placeholder="Message"></input><br />
+           <button>Send!</button>
+         </Card>
+       </Card>
+     </div>
+   )
+}
 }
 
 const mapStateToProps = reduxState => ({ reduxState, });
